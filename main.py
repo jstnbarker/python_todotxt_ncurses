9c81ac362afe4df9659604917851c162b486875b
Justin Barker <jstnbarker@pm.me>
2023-09-20 22:58:46 -0400

    begin article class

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import sys
 
+class article:
+    isComplete = False
+    description = ""
+    taglist = []
+
+    def __init__(self, isComplete, description, taglist, duedate):
+        self.isComplete = isComplete
+        self.description = description
+        self.duedate = duedate
+
 def read_file(file):
     with open(file, 'r', encoding="utf-8") as f:
         print(f.read())
         return f.read()
-
-def parse():
-
 
 if __name__ == '__main__':
     readlist=read_file(sys.argv[1])
